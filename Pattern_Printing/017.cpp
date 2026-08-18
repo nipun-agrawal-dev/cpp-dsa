adf4be01a90b66fe28dd5615b81920d70672c878
nipun-agrawal <315953084+nipun-agrawal-dev@users.noreply.github.com>
2026-08-18 23:37:24 +0530

    basic question

---
+++ Pattern_Printing/017.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char x = 'a';
+  for (int i = 1; i <= 5; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << x << " ";
+    }
+    x++;
+    cout << endl;
+  }
+
+  return 0;
+}
